3a4181da29ac5c121ac9e66f96871918d428c768
Adam <niklas.adam.andersson@gmail.com>
2018-09-07 18:51:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/adamandersson/workoutapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.adamandersson.workoutapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void enterHomePage(View view) {
+        Intent intent = new Intent(this, homePageActivity.class);
+        startActivity(intent);
+    }
+}
